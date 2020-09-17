b945d4f6cb2df96366baa7092911851bb22ef4d7
stefanvanderheijden <stefanvanderheijden@gmail.com>
2020-09-17 13:13:27 +0200

    added

--- main.py
+++ main.py
@@ -22,6 +22,6 @@ def loop():
 # Zet de GPIO pin als ingang.
 
 # Gebruik een interrupt, wanneer actief run subroutinne 'gedrukt'
-GPIO.add_event_detect(17, GPIO.RISING, callback=pinDetect, bouncetime=1)
+GPIO.add_event_detect(17, GPIO.RISING, callback=pinDetect, bouncetime=200)
  
 loop()
