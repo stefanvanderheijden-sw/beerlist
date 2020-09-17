0f61a5a158d53b9e829df5063bc5be464922efa8
stefanvanderheijden <stefanvanderheijden@gmail.com>
2020-09-17 13:09:44 +0200

    added

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("testing main script 2")
+import RPi.GPIO as GPIO
+from time import sleep
+ 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17, GPIO.IN)
+ 
+def pinDetect(pin):
+  print("pin pushed")
+ 
+def loop():
+  try:
+    raw_input()
+  # Wanneer er op CTRL+C gedrukt wordt.
+  except KeyboardInterrupt:  
+    # GPIO netjes afsluiten
+    GPIO.cleanup() 
+ 
+# Zet de GPIO pin als ingang.
+
+# Gebruik een interrupt, wanneer actief run subroutinne 'gedrukt'
+GPIO.add_event_detect(17, GPIO.RISING, callback=pinDetect, bouncetime=20)
+ 
+loop()
