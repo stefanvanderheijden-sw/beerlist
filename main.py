489cfab884e1aac838543f0f2e518e55491044ec
stefanvanderheijden <stefanvanderheijden@gmail.com>
2020-09-19 17:00:33 +0200

    henk

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ bottomButtonVar = tk.StringVar()
 rightTopButtonLabel = tk.Label(rightTopButtonFrame,textvariable=topButtonVar, width =  "15",height= "2", background="white")
 rightBottomButtonLabel = tk.Label(rightBottomButtonFrame,textvariable=bottomButtonVar, width =  "15",height= "2", background="white")
 
-rightColumn.pack(side="left")
+rightColumn.pack()
 
 rightTopButtonFrame.pack()
 rightBottomButtonFrame.pack()
